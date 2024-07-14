368444dceb0c2ee70684d9d9cce90a632f4d8605
Satendra Kumar Singh <43107630+Satendrakumarsingh@users.noreply.github.com>
2024-07-14 17:29:29 +0530

    Updates

---
+++ precache-manifest.a7fb3bff23c944903d6be5b0df4ddd3b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "750b9272b1103eaa2490233e6ae0fd54",
+    "url": "/textutils-react/index.html"
+  },
+  {
+    "revision": "0a371ec0816060c6f5d5",
+    "url": "/textutils-react/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "1864b30fda5ca4be335b",
+    "url": "/textutils-react/static/js/2.11236df4.chunk.js"
+  },
+  {
+    "revision": "60f6bf9e100e456690e9ab6c9a37bfc2",
+    "url": "/textutils-react/static/js/2.11236df4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d2294dc036c78a4b6b9f",
+    "url": "/textutils-react/static/js/3.d3041068.chunk.js"
+  },
+  {
+    "revision": "0a371ec0816060c6f5d5",
+    "url": "/textutils-react/static/js/main.6ffcd0a7.chunk.js"
+  },
+  {
+    "revision": "81d9b52812dabd7c2beb",
+    "url": "/textutils-react/static/js/runtime-main.8126db56.js"
+  }
+]);
