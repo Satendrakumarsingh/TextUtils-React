3708798345ee13788b928824b18ca68dadf77d5f
Satendra Kumar Singh <43107630+Satendrakumarsingh@users.noreply.github.com>
2024-07-14 17:26:03 +0530

    bug fixes

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,18 +6,18 @@ export default function Navbar(props) {
         <>
         <nav className={`navbar navbar-expand-lg navbar-${props.mode} bg-${props.mode}`}>
             <div className="container-fluid">
-                <a className="navbar-brand" href="/">{props.title}</a>
+                <a className="navbar-brand" href="#">{props.title}</a>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
                 </button>
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                     <li className="nav-item">
-                    {/* <Link className="nav-link active" aria-current="page" to="/">Home</Link> used in router*/}
+                    {/* <Link className="nav-link active" aria-current="page" to="/">Home</Link> */}
                     <a className="nav-link active" aria-current="page" href="#">Home</a>
                     </li>
                     <li className="nav-item">
-                    {/* <Link className="nav-link" to="/about">About</Link> used in router*/}
+                     {/* <Link className="nav-link" to="/about">About</Link> */}
                     <a className="nav-link" href="#">About</a>
                     </li>
                     <li className="nav-item dropdown">
@@ -32,7 +32,7 @@ export default function Navbar(props) {
                     </ul>
                     </li>
                     <li className="nav-item">
-                    <a className="nav-link disabled" aria-disabled="true">Disabled</a>
+                    <a className="nav-link disabled" aria-disabled="true" href='#'>Disabled</a>
                     </li>
                 </ul>
                 {/* <form className="d-flex" role="search">
@@ -46,7 +46,7 @@ export default function Navbar(props) {
                 </div>
                 <div className={`form-check form-switch text-${props.mode==='light'?'dark':'light'}`}>
                     <input className="form-check-input" onClick = {props.toggleMode} type="checkbox" id="flexSwitchCheckDefault"/>
-                    <label className="form-check-label" htmlFor="flexSwitchCheckDefault">Enable DarkMode </label>
+                    <label className="form-check-label" htmlFor="flexSwitchCheckDefault">Enable Dark Mode </label>
                 </div>
                 </div>
             </div>
